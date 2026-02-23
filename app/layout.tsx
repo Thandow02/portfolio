bd2ac39d553b62343ee7c602dd7fad4d2fa0b165
Mohau-Comfort <mahlakwane.mohau@gmail.com>
2026-02-23 09:20:59 +0200

    cleans project, removes any unused files and removes all console logs

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/thando.png" sizes="any" />
       </head>
       <body className={inter.className}>
         {children}
